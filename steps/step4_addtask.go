154812c24442e9d187cdf85a00b69ae416b9a6fe
Aleksandra Kulikova <Mugangovna@yandex.ru>
2024-09-12 23:42:18 +0300

    add const DateForFormat in some functions as argument, delete binding tag

--- steps/step4_addtask.go
+++ steps/step4_addtask.go
@@ -11,7 +11,7 @@ import (
 type Task struct {
 	ID      int64  `json:"id,string,omitempty"`
 	Date    string `json:"date,omitempty"`
-	Title   string `json:"title,omitempty" binding:"required"`
+	Title   string `json:"title,omitempty"`
 	Comment string `json:"comment,omitempty"`
 	Repeat  string `json:"repeat,omitempty"`
 	Error   string `json:"error,omitempty"`
@@ -36,19 +36,19 @@ func AddTaskWM(w http.ResponseWriter, r *http.Request) {
 
 	if task.Title == "" || task.Title == " " {
 		w.WriteHeader(http.StatusBadRequest)
-		writeInfo(w, Task{Error: "Не указан заголовок задачи"})
+		writeInfo(w, Task{Error: "не указан заголовок задачи"})
 		return
 	}
 
 	if task.Date == "" || task.Date == " " {
-		task.Date = time.Now().Format("20060102")
+		task.Date = time.Now().Format(DateForFormat)
 	}
 
 	if task.Date == "today" {
-		task.Date = time.Now().Format("20060102")
+		task.Date = time.Now().Format(DateForFormat)
 	}
 
-	parseDate, err := time.Parse("20060102", task.Date)
+	parseDate, err := time.Parse(DateForFormat, task.Date)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		writeInfo(w, Task{Error: err.Error()})
@@ -56,13 +56,13 @@ func AddTaskWM(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if parseDate.Before(time.Now()) {
-		if task.Repeat == "" || task.Repeat == " " || task.Date == time.Now().Format("20060102") {
-			task.Date = time.Now().Format("20060102")
+		if task.Repeat == "" || task.Repeat == " " || task.Date == time.Now().Format(DateForFormat) {
+			task.Date = time.Now().Format(DateForFormat)
 		} else {
 			task.Date, err = NextDateTask(time.Now(), task.Date, task.Repeat)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
-				writeInfo(w, Task{Error: "Oшибка функции вычисления даты выполнения задачи"})
+				writeInfo(w, Task{Error: "ошибка функции вычисления даты выполнения задачи"})
 				return
 			}
 		}
@@ -73,7 +73,7 @@ func AddTaskWM(w http.ResponseWriter, r *http.Request) {
 	insertId, err := Insert(DBConn, task)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		writeInfo(w, Task{Error: "Oшибка функции добавления записи в БД"})
+		writeInfo(w, Task{Error: "ошибка функции добавления записи в БД"})
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
